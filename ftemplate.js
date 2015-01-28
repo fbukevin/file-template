fa3f094002fbb392f5774b9dda05f8f995cb08af
Veck Hsiao <fbukevin@gmail.com>
2015-01-29 07:52:15 +0800

    finished filename check

--- ftemplate.js
+++ ftemplate.js
@@ -2,25 +2,44 @@
 
 var fs = require('fs');
 var program = require('commander');
-var colors = require('colors');
-var prompt = require('prompt');
+var match = require('minimatch');
+//var colors = require('colors');
+//var prompt = require('prompt');
 
-var htmlHandler = require('./html')
-var javaHandler = require('./java')
-var cHandler = require('./c')
+//var htmlHandler = require('./html')
+//var javaHandler = require('./java')
+//var cHandler = require('./c')
 
 program
 	.version('0.0.1')
-
+/*
 program
 	.command('')
 	.option()
 	.option()
+*/
+program
+	.usage('<filename>.<extension> [option]');
+
 
 /* create command parser */
 var parser = program.parse(process.argv);
 
 /* check for the command line arguments */
-if (parser.args.length === 0) {
-    parser.outputHelp();
+if (parser.args.length === 0) 
+	parser.outputHelp();
+else if(parser.args[0]) 	
+{
+	/*
+	//DEBUG
+	process.argv.forEach(function(str){ console.log(str);});	// include "node" and "filename"
+	console.log('\n======\n');
+	parser.args.forEach(function(str){ console.log(str);});		// exclude "node" and "filename"
+	*/	
+	var filename = parser.args[0];
+	if(!match(filename, '*.*'))
+		program.outputHelp();
+
 }
+
+console.log(filename);
